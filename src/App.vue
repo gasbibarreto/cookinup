e0b525afa1028db39fbb215d6ca2a690dccc9c40
Gabriella Soares <gabriellakbsoares@gmail.com>
2025-08-04 21:22:06 -0300

    insert conteudo principal component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import Banner from './components/Banner.vue';
+import Principal from './components/Principal.vue';
 
 
 </script>
 
 <template>
   <Banner />
+  <Principal />
 
 </template>
 
