14ffbc9296e4334054e7e53fe10a565f1e6aa0a5
Gabriella Soares <gabriellakbsoares@gmail.com>
2025-08-25 08:58:07 -0300

    incluindo mostrar receitas

--- src/components/MostrarReceitas.vue
+++ src/components/MostrarReceitas.vue
@@ -2,15 +2,29 @@
 import type IReceitas from "@/interfaces/iReceitas";
 import BotaoPrincipal from "./BotaoPrincipal.vue";
 import { obterReceitas } from "@/http";
+import type { PropType } from "vue";
 
 export default {
+    props: {
+        ingredientes: {
+            type: Array as PropType<string[]>,
+            required: true
+        }
+    },
     data() {
         return {
-            receitas: [] as IReceitas[]
+            receitas: [] as IReceitas[],
         }
     },
     async created() {
-        this.receitas = await obterReceitas();
+        const buscarReceitas = await obterReceitas();
+
+        this.receitas = buscarReceitas.filter(receita => {
+            return receita.ingredientes.some(ingrediente => 
+                this.ingredientes.includes(ingrediente)
+            )
+        });
+
     },
     emits: ['selecionarIngredientes'],
     components: { BotaoPrincipal }
@@ -18,23 +32,35 @@ export default {
 </script>
 
 <template>
-    <section>
+    <section class="mostrar-receitas">
         <h1 class="cabecalho titulo-receitas">
             Receitas
         </h1>
-        <p class="paragrafo-lg resultado">
-            Resultados encontrados:
+        <p class="paragrafo-lg resultados">
+            Resultados encontrados: {{ receitas.length }}
         </p>
-        <p class="paragrafo-lg opcoes">
+        <p class="paragrafo-lg informacoes">
             Veja as opcoes de receitas com os ingredientes que voce tem por ai!
         </p>
-        <ul class="receitas">
-            <li v-for="receita in receitas" :key="receita.nome">
-                <img :src="`/imagens/receitas/${receita.imagem}`" alt="Icones de imagens de cada receita"
-                    class="recceitas__imagem" />
-                <h2 class="paragrafo-lg receitas__nome">{{ receita.nome }}</h2>
-            </li>
-        </ul>
+        <div v-if="receitas.length">
+            <ul class="receitas">
+                <li v-for="receita in receitas" :key="receita.nome">
+                    <img :src="`/imagens/receitas/${receita.imagem}`" alt="Icones de imagens de cada receita"
+                        class="recceitas__imagem" />
+                    <h2 class="paragrafo-lg receitas__nome">{{ receita.nome }}</h2>
+                </li>
+            </ul>
+        </div>
+        <div v-else class="receitas-nao-encontradas">
+            <p class="paragrafo-lg receitas-nao-encontradas__info">
+                Ops, não encontramos resultados para sua combinação. Vamos tentar de novo?
+            </p>
+
+            <img src="../assets/imagens/sem-receitas.png"
+                alt="Desenho de um ovo quebrado. A gema tem um rosto com uma expressão triste.">
+
+
+        </div>
 
     </section>
 
@@ -43,23 +69,64 @@ export default {
 </template>
 
 <style scoped>
+.mostrar-receitas {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+}
+
+.resultados {
+    color: var(--verde-medio, #3D6D4A);
+    margin-bottom: 0.5rem;
+}   
 
 .titulo-receitas {
     color: var(--verde-medio, #3D6D4A);
     display: block;
     margin-bottom: 1.5rem;
 }
-  
+
+.informacoes {
+    margin-bottom: 2rem;
+}
+
 .receitas {
-  margin-bottom: 1rem;
-  display: flex;
-  justify-content: center;
-  gap: 1.5rem;
-  flex-wrap: wrap;
+    margin-bottom: 2rem;
+    display: flex;
+    justify-content: center;
+    gap: 1.5rem;
+    flex-wrap: wrap;
+}
+
+.receitas li {
+    padding-bottom: 2.5rem;
+    border-radius: 1rem;
+    background: var(--branco, #FFF);
+    box-shadow: 4px 4px 10px 0px rgba(68, 68, 68, 0.05);
+    height: 100%;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 2rem;
+}
+
+.receitas h2 {
+    font-size: medium;
 }
 
 .recceitas__imagem {
-  width: 20rem;
+    width: 20rem;
+    border-top-right-radius: 1rem;
+    border-top-left-radius: 1rem;
 }
 
+.receitas-nao-encontradas {
+    margin-bottom: 2rem;
+}
+
+.receitas-nao-encontradas__info {
+    margin-bottom: 0.5rem;
+}
 </style>
--- src/components/Principal.vue
+++ src/components/Principal.vue
@@ -5,24 +5,24 @@ import MostrarReceitas from "./MostrarReceitas.vue";
 
 type Pagina = 'SelecionarIngredientes' | 'MostrarReceitas';
 
-export default{
+export default {
     data() {
         return {
             ingredientes: [] as string[],
             conteudo: 'SelecionarIngredientes' as Pagina
         };
     },
-    methods:{
+    methods: {
         adicionarIngrediente(ingrediente: string) {
             this.ingredientes.push(ingrediente)
         },
-        
+
         removerIngrediente(ingrediente: string) {
-            const newIngredientes = this.ingredientes.filter( el => 
-                ingrediente !== el 
+            const newIngredientes = this.ingredientes.filter(el =>
+                ingrediente !== el
             )
 
-            this.ingredientes = newIngredientes            
+            this.ingredientes = newIngredientes
         },
         navegarPagina(pagina: Pagina) {
             this.conteudo = pagina
@@ -31,36 +31,34 @@ export default{
     emits: ['adicionarIngrediente', 'removerIngrediente', 'buscarReceitas', 'selecionarIngredientes'],
     components: { SelecionarIngredientes, SuaLista, MostrarReceitas }
 }
-    
+
 </script>
 
 <template>
-<main class="conteudo-principal">
-    <SuaLista :ingredientes= "ingredientes" />
+    <main class="conteudo-principal">
+        <SuaLista :ingredientes="ingredientes" />
+        <KeepAlive>
+            <SelecionarIngredientes v-if="conteudo === 'SelecionarIngredientes'"
+                @adicionar-ingrediente="adicionarIngrediente" @remover-ingrediente="removerIngrediente"
+                @buscar-receitas="navegarPagina('MostrarReceitas')" />
 
-    <SelecionarIngredientes v-if="conteudo === 'SelecionarIngredientes'"
-        @adicionar-ingrediente="adicionarIngrediente"
-        @remover-ingrediente="removerIngrediente"
-        @buscar-receitas="navegarPagina('MostrarReceitas')"
-        />
-    
-    <MostrarReceitas v-else-if="conteudo === 'MostrarReceitas'"  @selecionar-ingredientes="navegarPagina('SelecionarIngredientes')" />
-</main>         
+            <MostrarReceitas v-else-if="conteudo === 'MostrarReceitas'"
+                :ingredientes="ingredientes"
+                @selecionar-ingredientes="navegarPagina('SelecionarIngredientes')" />
+        </KeepAlive>
+    </main>
 </template>
 
 <style scoped>
 .conteudo-principal {
-  padding: 6.5rem 7.5rem;
-  border-radius: 3.75rem 3.75rem 0rem 0rem;
-  background: var(--creme, #FFFAF3);
-  color: var(--cinza, #444);
+    padding: 6.5rem 7.5rem;
+    border-radius: 3.75rem 3.75rem 0rem 0rem;
+    background: var(--creme, #FFFAF3);
+    color: var(--cinza, #444);
 
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 5rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 5rem;
 }
-
-
-
 </style>
